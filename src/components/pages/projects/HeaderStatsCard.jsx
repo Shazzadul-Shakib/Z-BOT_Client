9194dd2b3fa28e61a2816e9961869022886af690
Shakib-Shazzadul <shakib1186@gmail.com>
2024-07-27 01:27:12 +0600

    chart added in project header stats

--- src/components/pages/projects/HeaderStatsCard.jsx
+++ src/components/pages/projects/HeaderStatsCard.jsx
@@ -1,37 +1,105 @@
 import { Card, CardContent } from "@/components/ui/card";
-
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Label, Pie, PieChart } from "recharts";
 
 const HeaderStatsCard = () => {
-    return (
-      <main className="">
-        <Card className="col-span-1">
-          <CardContent className="grid p-4">
-            <div className="grid items-center gap-2">
-              <div className="grid flex-1 auto-rows-min gap-0.5">
-                <div className="text-sm text-muted-foreground">
-                  All Projects
-                </div>
-                <div className="flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none">
-                  5
-                </div>
-              </div>
-              <div className="grid flex-1 auto-rows-min gap-0.5">
-                <div className="text-sm text-muted-foreground">Finished</div>
-                <div className="flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none">
-                  4
-                </div>
-              </div>
-              <div className="grid flex-1 auto-rows-min gap-0.5">
-                <div className="text-sm text-muted-foreground">On Going</div>
-                <div className="flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none">
-                  1
-                </div>
+  const chartData = [
+    { projects: "done", visitors: 4, fill: "hsl(var(--chart-1))" },
+    { projects: "onGoing", visitors: 1, fill: "hsl(var(--chart-2))" },
+  ];
+
+  const chartConfig = {
+    visitors: {
+      label: "Visitors",
+    },
+    done: {
+      label: "done",
+      color: "hsl(var(--chart-1))",
+    },
+    onGoing: {
+      label: "onGoing",
+      color: "hsl(var(--chart-2))",
+    },
+  };
+
+  return (
+    <main className="">
+      <Card className="col-span-1">
+        <CardContent className="grid grid-cols-2 p-4 gap-4">
+          {/* Stats */}
+          <div className="col-span-1 grid items-center gap-2">
+            <div className="grid flex-1 auto-rows-min gap-0.5">
+              <div className="text-sm text-muted-foreground">All Projects</div>
+              <div className="flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none">
+                5
               </div>
             </div>
-          </CardContent>
-        </Card>
-      </main>
-    );
+            <div className="grid flex-1 auto-rows-min gap-0.5">
+              <div className="text-sm text-muted-foreground">Finished</div>
+              <div className="flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none">
+                4
+              </div>
+            </div>
+            <div className="grid flex-1 auto-rows-min gap-0.5">
+              <div className="text-sm text-muted-foreground">On Going</div>
+              <div className="flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none">
+                1
+              </div>
+            </div>
+          </div>
+
+          {/* Chart */}
+          <div className="col-span-1 flex justify-center items-center">
+            <ChartContainer
+              config={chartConfig}
+              className="aspect-square w-full max-w-[150px] min-w-[130px]"
+            >
+              <PieChart>
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie
+                  data={chartData}
+                  dataKey="visitors"
+                  nameKey="projects"
+                  innerRadius={35}
+                  strokeWidth={5}
+                >
+                  <Label
+                    content={({ viewBox }) => {
+                      if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                        return (
+                          <text
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                          >
+                            <tspan
+                              x={viewBox.cx}
+                              y={viewBox.cy}
+                              className="fill-foreground text-2xl font-bold"
+                            >
+                              {"5".toLocaleString()}
+                            </tspan>
+                          </text>
+                        );
+                      }
+                    }}
+                  />
+                </Pie>
+              </PieChart>
+            </ChartContainer>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
 };
 
 export default HeaderStatsCard;
