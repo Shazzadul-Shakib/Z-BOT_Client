47eee6d99bdbbe1b31284ed455514774b80d3605
Shakib-Shazzadul <shakib1186@gmail.com>
2024-07-31 21:16:12 +0600

    added add new task field

---
+++ src/components/modals/AddTaskModal.jsx
@@ -0,0 +1,19 @@
+// import { PlusCircle } from "lucide-react";
+// import { Button } from "../ui/button";
+// import { Card, CardContent, CardHeader } from "../ui/card";
+// import { Input } from "../ui/input";
+
+// const AddTaskModal = () => {
+//     return (
+//       <div>
+//         <Card className="max-w-[700px]">
+//           <CardHeader>
+//             <h1 className="text-xl font-semibold text-center">Add New Task</h1>
+//           </CardHeader>
+          
+//         </Card>
+//       </div>
+//     );
+// };
+
+// export default AddTaskModal;
--- src/components/pages/project/TaskModules.jsx
+++ src/components/pages/project/TaskModules.jsx
@@ -7,6 +7,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Table,
@@ -25,59 +26,68 @@ const invoices = [
     paymentStatus: "Paid",
     totalAmount: "$250.00",
     paymentMethod: "Credit Card",
-  }
+  },
 ];
 
 const TaskModules = () => {
   return (
-    <Card className="h-[80dvh] overflow-y-auto hide-scrollbar">
-      <Table>
-        <TableHeader className="bg-primary-foreground">
-          <TableRow>
-            <TableHead className="w-[100px]">STATUS</TableHead>
-            <TableHead>TASKS</TableHead>
-            <TableHead className="hidden md:table-cell">DATE</TableHead>
-            <TableHead className="text-right">MANAGE</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">
-                <Checkbox id={`terms-${invoice.invoice}`} />
-              </TableCell>
-              <TableCell className="font-medium">
-                <Label htmlFor={`terms-${invoice.invoice}`}>
-                  Details about the modular task to be done.
-                </Label>
-              </TableCell>
-              <TableCell className="hidden md:table-cell">
-                November 23, 2023
-              </TableCell>
-              <TableCell className="font-medium text-right">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button size="icon" variant="outline" className="h-8 w-8">
-                      <MoreVertical className="h-3.5 w-3.5" />
-                      <span className="sr-only">More</span>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Delete</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
+    <Card className="h-[86dvh] relative">
+      <div className="overflow-y-auto h-[calc(86dvh-72px)]">
+        <Table>
+          <TableHeader className="bg-primary-foreground">
+            <TableRow>
+              <TableHead className="w-[100px]">STATUS</TableHead>
+              <TableHead>TASKS</TableHead>
+              <TableHead className="hidden md:table-cell">DATE</TableHead>
+              <TableHead className="text-right">MANAGE</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <CardFooter className="justify-center border-t p-4">
-        <Button size="sm" variant="secondary" className="gap-1">
-          <PlusCircle className="h-3.5 w-3.5" />
-          Add New Task
-        </Button>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className="font-medium">
+                  <Checkbox id={`terms-${invoice.invoice}`} />
+                </TableCell>
+                <TableCell className="font-medium">
+                  <Label htmlFor={`terms-${invoice.invoice}`}>
+                    Details about the modular task to be done.
+                  </Label>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  November 23, 2023
+                </TableCell>
+                <TableCell className="font-medium text-right">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button size="icon" variant="outline" className="h-8 w-8">
+                        <MoreVertical className="h-3.5 w-3.5" />
+                        <span className="sr-only">More</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <CardFooter className="absolute bottom-0 left-0 right-0 p-4 bg-primary-foreground">
+        <div className="flex w-full items-center space-x-2">
+          <Input
+            type="text"
+            className="focus-visible:ring-1 py-4"
+            placeholder="Enter Your New Task..."
+          />
+          <Button type="submit" className="gap-2">
+            <PlusCircle className="h-5 w-5" />
+            <span className="hidden md:block">Add Task</span>
+          </Button>
+        </div>
       </CardFooter>
     </Card>
   );
