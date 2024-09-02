2da90e5f55f3e1b169944e12fe156361fbe92672
Shakib-Shazzadul <shakib1186@gmail.com>
2024-09-02 11:40:49 +0600

    register page form validation with hook form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,39 +4,76 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  
     const [passwordShown, setPasswordShown] = useState(false);
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => console.log(data);
 
     const togglePasswordVisibility = () => {
       setPasswordShown(!passwordShown);
     };
 
   return (
-    <Card className="mx-auto w-[400px]">
+    <Card className="mx-auto w-[350px] md:w-[400px]">
       <CardHeader>
         <CardTitle className="text-xl mb-2">Sign Up</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="name">User Name</Label>
-            <Input id="name" placeholder="Enter your name" required />
+            <Input
+              {...register("userName", { required: true })}
+              id="name"
+              placeholder="Enter your name"
+            />
+            {errors.userName && (
+              <span className=" text-xs text-destructive ">
+                Name is required*
+              </span>
+            )}
           </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              {...register("userEmail", { required: true })}
               id="email"
               type="email"
               placeholder="m@example.com"
-              required
             />
+            {errors.userEmail && (
+              <span className=" text-xs text-destructive ">
+                Email is required*
+              </span>
+            )}
           </div>
           <div className="grid gap-2 ">
             <Label htmlFor="password">Password</Label>
             <div className="relative">
-              <Input id="password" type={passwordShown ? "text" : "password"} />
+              <Input
+                {...register("password", {
+                  required: "Password is required*",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters*",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "Password must not exceed 20 characters*",
+                  },
+                })}
+                id="password"
+                type={passwordShown ? "text" : "password"}
+              />
               <div
                 className=" absolute right-3 top-2.5 cursor-pointer"
                 onClick={togglePasswordVisibility}
@@ -44,11 +81,16 @@ const Register = () => {
                 {passwordShown ? <EyeOff size={20} /> : <Eye size={20} />}
               </div>
             </div>
+            {errors.password && (
+              <span className=" text-xs text-destructive ">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <Button type="submit" className="w-full">
             Create an account
           </Button>
-        </div>
+        </form>
         <div className="mt-4 text-center text-sm">
           Already have an account?{" "}
           <Link to="/login" className="underline">
